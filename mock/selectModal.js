308e7266b0c0f44e811778b60b9c259129bbc32c
janeluck <janeluck158@gmail.com>
2017-04-01 13:58:56 +0800

    mock data

--- mock/selectModal.js
+++ mock/selectModal.js
@@ -6,94 +6,109 @@ const Mock = require('mockjs')
 import mockStorge from '../src/utils/mockStorge'
 
 let dataKey = mockStorge('SelectModal', Mock.mock({
+  deptTree: {
+    'Name': '全公司',
+    'ID': '0',
+    'Code': '000001',
+    'StopFlag': '0',
+    'Children|5': [
+      {
+        'ID|+1': 1,
+        Name: '@cword(5, 7)',
+        //nickName: '@last',
+        Code: /^[123456789]\d{6}$/,
+        StopFlag: '0',
+        'Children|5': [
+          {
+            'ID|+1': 11,
+            Name: '@cword(2, 5)',
+            //nickName: '@last',
+            Code: /^[123456789]\d{6}$/,
+            StopFlag: '0',
+            'Children': [
 
-  'Name': '全公司',
-  'ID': '0',
-  'Code': '000001',
-  'StopFlag': '0',
-  'Children|5': [
+              {
+                'ID|+1': 111,
+                Name: '@cword(5, 7)',
+                //nickName: '@last',
+                Code: /^[123456789]\d{6}$/,
+                StopFlag: '0',
+                'Children|5': [
+                  {
+                    'ID|+1': 222,
+                    Name: '@cword(5, 7)',
+                    //nickName: '@last',
+                    Code: /^[123456789]\d{6}$/,
+                    StopFlag: '0',
+                  },
+
+
+                ]
+              },
+
+              {
+                'ID|+1': 33,
+                Name: '@cword(2, 5)',
+                //nickName: '@last',
+                Code: /^[123456789]\d{6}$/,
+                StopFlag: '0',
+                'Children|5': [
+
+                  {
+                    'ID|+1': 333,
+                    Name: '@cword(5, 7)',
+                    //nickName: '@last',
+                    Code: /^[123456789]\d{6}$/,
+                    StopFlag: '0',
+                    'Children|5': [
+                      {
+                        'ID|+1': 444,
+                        Name: '@cword(5, 7)',
+                        //nickName: '@last',
+                        Code: /^[123456789]\d{6}$/,
+                        StopFlag: '1',
+                      },
+
+
+                    ]
+                  },
+
+                ]
+              },
+            ]
+          },
+
+        ]
+      },
+
+    ],
+  },
+  'users|10': [
     {
-      'ID|+1': 1,
-      Name: '@cword(5, 7)',
-      //nickName: '@last',
-      Code: /^[123456789]\d{6}$/,
-      StopFlag: '0',
-      'Children|5': [
-        {
-          'ID|+1': 11,
-          Name: '@cword(2, 5)',
-          //nickName: '@last',
-          Code: /^[123456789]\d{6}$/,
-          StopFlag: '0',
-          'Children': [
+      "ID|+1": 1,
+      "Name": "@cname",
+      Avatar () {
+        return Mock.Random.image('100x100', Mock.Random.color(), '#757575', 'png', this.Name.substr(0, 1))
+      },
+    }
+  ]
 
-            {
-              'ID|+1': 111,
-              Name: '@cword(5, 7)',
-              //nickName: '@last',
-              Code: /^[123456789]\d{6}$/,
-              StopFlag: '0',
-              'Children|5': [
-                {
-                  'ID|+1': 222,
-                  Name: '@cword(5, 7)',
-                  //nickName: '@last',
-                  Code: /^[123456789]\d{6}$/,
-                  StopFlag: '0',
-                },
-
-
-              ]
-            },
-
- {
-          'ID|+1': 33,
-          Name: '@cword(2, 5)',
-          //nickName: '@last',
-          Code: /^[123456789]\d{6}$/,
-          StopFlag: '0',
-          'Children|5': [
-
-            {
-              'ID|+1': 333,
-              Name: '@cword(5, 7)',
-              //nickName: '@last',
-              Code: /^[123456789]\d{6}$/,
-              StopFlag: '0',
-              'Children|5': [
-                {
-                  'ID|+1': 444,
-                  Name: '@cword(5, 7)',
-                  //nickName: '@last',
-                  Code: /^[123456789]\d{6}$/,
-                  StopFlag: '1',
-                },
-
-
-              ]
-            },
-
-
-          ]
-        },
-          ]
-        },
-
-
-
-      ]
-    },
-
-  ],
 }))
 
-let deptTreeData = global[dataKey]
+let deptTreeData = global[dataKey]['deptTree']
+let users = global[dataKey]['users']
+
 
 module.exports = {
 
   'GET /api/deptTree' (req, res) {
 
     res.json({success: true, data: deptTreeData})
-  }
+  },
+
+  'GET /api/personList' (req, res) {
+
+    res.json({success: true, data: users})
+  },
 
 }
--- src/components/businessComponents/SelectModal/SelectModal.js
+++ src/components/businessComponents/SelectModal/SelectModal.js
@@ -2,8 +2,20 @@
  * Created by janeluck on 3/31/17.
  */
 import React, {PropTypes} from 'react'
-import {Modal, Input, Button} from 'antd'
+import {Modal, Input, Button, Tree, Checkbox} from 'antd'
 import Immutable from 'immutable'
+import reqwest from 'reqwest'
+
+
+const TreeNode = Tree.TreeNode
+const generatorTree = (data) => {
+  if (data.Children && data.Children.length != 0) {
+    return <TreeNode key={data.ID} title={data.Name}>
+      {data.Children.map(generatorTree)}
+    </TreeNode>
+  }
+  return <TreeNode key={data.ID} title={data.Name}/>
+}
 
 class SelectModal extends React.Component {
   constructor(props) {
@@ -12,26 +24,56 @@ class SelectModal extends React.Component {
 
       loading: false,
       selectedDeptID: '',
-      deptTree: [{}],
-
+      deptTree: {
+        Name: '',
+        ID: 0
+      },
       users: [],
-
       keyword: '',
       selectedKeys: []
-
     }
-
   }
 
-  componentDidMount() {
-    //this.getTreeData()
-    // todo 是否需要留有接口
-    // this.getChildren(0)
+  componentWillMount() {
+    this.getDeptTree()
   }
 
+  getDeptTree = () => {
+    const that = this
+    reqwest({
+      url: location.origin + '/api/deptTree',
+      type: 'json'
+    }).then(data=> {
+      that.setState({
+        deptTree: data.data
+      })
+    })
+  }
+
+  getList = (id) => {
+    const that = this
+    reqwest({
+      url: location.origin + '/api/personList',
+      type: 'json'
+    }).then(data=> {
+      that.setState({
+        users: data.data
+      })
+    })
+  }
+
+
+  onSelect = (selectedKeys, e) => {
+    //selected: bool, selectedNodes, node, event
+    const id = e.node.props.eventKey
+    this.getList(id)
+
+  }
 
   render() {
 
+
+    const {deptTree, users} = this.state
     return (
       <div>
 
@@ -48,7 +90,7 @@ class SelectModal extends React.Component {
             {/*--搜索栏--*/}
             <div>
               <span>搜索人员:</span>
-              <Input  style={{width: 200}}/>
+              <Input style={{width: 200}}/>
               <Button type="primary">搜索</Button>
 
             </div>
@@ -57,22 +99,54 @@ class SelectModal extends React.Component {
 
             {/*--已选择--*/}
             <div>
-                <div>已选择</div>
-                <div>
+              <div>已选择</div>
+              <div>
 
-                </div>
+              </div>
 
             </div>
             {/*--已选择--*/}
 
 
-
-
             {/*--部门人员双栏--*/}
 
             <div>
+              <div>
                 <div>部门</div>
+                <div>
+
+                  <Tree
+                    onSelect={this.onSelect}
+
+                  >
+                    {generatorTree(deptTree)}
+                  </Tree>
+
+
+                </div>
+
+              </div>
+
+              <div>
                 <div>人员</div>
+
+                <div>
+
+                    <ul>
+                      {users.map(user=><li key={user.ID}>
+                        <Checkbox />
+
+                        {user.Name}
+
+
+                        </li>)}
+
+                    </ul>
+
+
+                </div>
+              </div>
+
 
             </div>
             {/*--部门人员双栏--*/}
