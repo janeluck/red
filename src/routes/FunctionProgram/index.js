bdd51d6ac98b7e90c29b6ee39187659fd6da4cd4
janeluck <janeluck158@gmail.com>
2017-04-02 13:35:01 +0800

    display table

--- src/routes/FunctionProgram/index.js
+++ src/routes/FunctionProgram/index.js
@@ -7,17 +7,25 @@ import _ from 'lodash'
 
 const lameCSV = (str) => {
   return _.reduce(str.split('\n'), function (table, row) {
-     table.push(_.map(row.split(','), function (c) {
+    table.push(_.map(row.split(','), function (c) {
       return c.trim()
     }))
-     return table
+    return table
   }, [])
 }
 const str = 'name,age,hair\nMerble,35,red\nBob,64,blonde'
 const FP = () => <div>
   <div>string: {str}</div>
 
-  <div>{lameCSV(str)}</div>
+
+  <div>   {lameCSV(str)}</div>
+  <table>
+    <tbody>
+
+    {lameCSV(str).map((tr, i) => <tr key={i}>{tr.map((td, j) => <td key={j}>{td}</td>)}</tr>)}
+
+    </tbody>
+  </table>
 </div>
 
 export default FP
