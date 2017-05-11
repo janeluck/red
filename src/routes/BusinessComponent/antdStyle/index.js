f3a3227f8299ee15ceb9c24b701075631c9c91d7
janeluck <janeluck158@gmail.com>
2017-05-11 16:47:51 +0800

    line-height

--- src/routes/BusinessComponent/antdStyle/index.js
+++ src/routes/BusinessComponent/antdStyle/index.js
@@ -5,10 +5,7 @@
 import React from 'react'
 import {Row, Col, Select, Modal, Button, Input, Tag} from 'antd'
 const Option = Select.Option;
-
-
-
-
+import styles from './index.less'
 
 
 class AntdStylePage extends React.Component {
@@ -23,10 +20,24 @@ class AntdStylePage extends React.Component {
 
     return (<div className="content-inner">
 
-      <h2 style={{margin: '16px 0'}}>antd component demo</h2>
+        <h2 style={{margin: '16px 0'}}>antd style demo</h2>
+        <div>
+          pagination
+          <div>
+            <ul className={styles['jane-pagination']}>
+              <li className={styles['jane-pagination-item']}><a>1</a></li>
+              <li className={styles['jane-pagination-item']}><a>1</a></li>
+              <li className={styles['jane-pagination-item']}><a>1</a></li>
+            </ul>
 
 
-    </div>)
+            <div style={{height: 100, marginBottom: -75, width: 100, backgroundColor: 'red'}}>A</div>
+            <div style={{height: 100, marginTop: -50, marginLeft: 50, width: 100, backgroundColor: 'green'}}>B</div>
+          </div>
+
+        </div>
+      </div>
+    )
   }
 }
 
