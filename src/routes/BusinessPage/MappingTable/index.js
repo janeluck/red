5d4b87e46c12d074400c22deb4cca9bd3aa68032
janeluck <janeluck158@gmail.com>
2017-06-06 16:35:29 +0800

    autoCorelated

--- src/routes/BusinessPage/MappingTable/index.js
+++ src/routes/BusinessPage/MappingTable/index.js
@@ -3,16 +3,113 @@
  */
 import React from 'react'
 import _ from 'lodash'
+import Immutable from 'immutable'
+import style from './index.less'
+import classNames from 'classnames';
+
+const data = [
+  {
+    Name: 'alex',
+    Phone: '13512345678',
+    Mail: 'alex@qq.com',
+    U: [{
+      Name: 'alex',
+      Phone: '13512345678',
+      Mail: 'alex@qq.com',
+      IsCorelated: 1
+    }],
+    AutoCorelated: 0
+  },
+  {
+    Name: 'lucy',
+    Phone: '13612345678',
+    Mail: 'lucy@qq.com',
+    U: [{
+      Name: 'lucy',
+      Phone: '13512345678',
+      Mail: 'lucy@qq.com',
+      IsCorelated: 1
+    }, {
+      Name: 'lucy002',
+      Phone: '13522345678',
+      Mail: 'lucy002@qq.com',
+      IsCorelated: 0
+    }, {
+      Name: 'lucy003',
+      Phone: '13532345678',
+      Mail: 'lucy003@qq.com',
+      IsCorelated: 0
+    },],
+    AutoCorelated: 0
+  },
+  {
+    Name: 'tom',
+    Phone: '13812345678',
+    Mail: 'alex@qq.com',
+    U: [{
+      Name: 'tom',
+      Phone: '13812345678',
+      Mail: 'tom@qq.com',
+      IsCorelated: 1
+    }],
+    AutoCorelated: 1
+  },
+]
+
+
 class MappingTablePage extends React.Component {
   constructor(props) {
     super(props)
+    this.state = {
+      $$data: Immutable.fromJS(data)
+    }
+  }
+
+  renderRow = (row, rowIndex) => {
+    const u = row.U
+    const rowSpan = u.length
+
+    if (rowSpan == 1) {
+      const utdCls = {
+        [style.autoCorelated]: row.AutoCorelated == 1
+      }
+      return (<tr key={`${rowIndex}-0`}>
+        <td>
+          {row.Name}
+        </td>
+        <td>
+          {row.Phone}
+        </td>
+        <td>
+          {row.Mail}
+        </td>
+        <td className={classNames(utdCls)}>
+          {u[0].Name}
+        </td>
+        <td className={classNames(utdCls)}>
+          {u[0].Phone}
+        </td>
+        <td className={classNames(utdCls)}>
+          {u[0].Mail}
+        </td>
+        <td>
+          ok
+        </td>
+      </tr>)
+    } else {
+
+
+    }
+
+
   }
 
   render() {
 
+    const data = this.state.$$data.toJS()
     return (
       <div style={{padding: '30px'}}>
-        <table>
+        <table className={style.mappingTable}>
           <thead>
           <tr>
             <th colSpan="3">超客</th>
@@ -30,6 +127,9 @@ class MappingTablePage extends React.Component {
           </tr>
           </thead>
           <tbody>
+
+          {_.map(data, this.renderRow)}
+
           <tr >
             <td rowSpan="3">
               alex
@@ -43,32 +143,39 @@ class MappingTablePage extends React.Component {
             </td>
             <td>
               wang001
-            </td><td>
+            </td>
+            <td>
               13512345678
-            </td><td>
+            </td>
+            <td>
               wang001@qq.com
             </td>
             <td>重新选择</td>
           </tr>
-         <tr>
+          <tr>
             <td>
               wang002
-            </td><td>
+            </td>
+            <td>
               13512345677
-            </td><td>
+            </td>
+            <td>
               wang002@qq.com
             </td>
-           <td></td>
-         </tr><tr>
+            <td></td>
+          </tr>
+          <tr>
             <td>
               wang003
-            </td><td>
+            </td>
+            <td>
               13512345671
-            </td><td>
+            </td>
+            <td>
               wang003@qq.com
             </td>
             <td></td>
-         </tr>
+          </tr>
           </tbody>
         </table>
 
